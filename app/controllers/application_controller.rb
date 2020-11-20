e9f0731d1402af0b6d74c2a73201adafc3734e66
moriken4320 <moriken.4320@gmail.com>
2020-11-21 00:19:51 +0900

    保存に不要なパラム削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   private
  
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :provider, :uid, :image])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :image])
   end
 
   #ログイン状態であれば、recruitments_path(index)へリダイレクト
