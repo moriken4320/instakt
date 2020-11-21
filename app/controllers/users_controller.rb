661b54445d06be8dd13ade5c219a0b80d40d9d4c
moriken4320 <moriken.4320@gmail.com>
2020-11-21 19:14:16 +0900

    トップ画面のフロント実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   # before_action :signed_in_user_to_recruitments, only: [:top]
-  before_action :signed_out_to_root, except: [:top]
+  before_action :signed_out_to_root
 
   def my_profile
     @user = current_user
