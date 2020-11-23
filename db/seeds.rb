e1600470320f40b156704e9317756a106458cc5f
moriken4320 <moriken.4320@gmail.com>
2020-11-23 23:04:36 +0900

    seed作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+20.times do |i|
+  user = User.new(name: i, email: "#{i}@gmail.com", password: "aaaaaa", password_confirmation: "aaaaaa", provider: "aaa", uid: "aaa", image: "")
+  user.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+  user.save
+end
