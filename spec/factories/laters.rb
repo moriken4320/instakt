1c23bda65cfbbac6c263d0f53bf3358a1b32f204
moriken4320 <moriken.4320@gmail.com>
2020-11-28 01:14:04 +0900

    laterモデルの単体テストコード作成

--- spec/factories/laters.rb
+++ spec/factories/laters.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
   factory :later do
-    
+    association :recruit
   end
 end
--- spec/models/later_spec.rb
+++ spec/models/later_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Later, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @later = FactoryBot.build(:later)
+  end
+
+  context "「これから」の募集内容が保存可能" do
+    it "recruitが正常に存在すると保存できる" do
+      expect(@later).to be_valid 
+    end
+  end
+
+  context "「これから」の募集内容が保存不可" do
+    it "recruitが正常に存在しないと保存できない" do
+      @later.recruit = nil
+      @later.valid?
+      expect(@later.errors.full_messages).to include("Recruit must exist") 
+    end
+  end
 end
