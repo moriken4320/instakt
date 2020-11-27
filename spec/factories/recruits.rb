aafa38532f4abc4c9e2fa17bd07b4255ef759f8f
moriken4320 <moriken.4320@gmail.com>
2020-11-28 01:13:36 +0900

    recruitモデルの単体テストコード作成

--- spec/factories/recruits.rb
+++ spec/factories/recruits.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
   factory :recruit do
-    
+    association :user
   end
 end
--- spec/models/recruit_spec.rb
+++ spec/models/recruit_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Recruit, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @recruit = FactoryBot.build(:recruit)
+  end
+
+  context "募集情報が保存可能" do
+    it "userが正常に存在すると保存できる" do
+      expect(@recruit).to be_valid 
+    end
+  end
+
+  context "募集情報が保存不可" do
+    it "userが正常に存在しないと保存できない" do
+      @recruit.user = nil
+      @recruit.valid?
+      expect(@recruit.errors.full_messages).to include("User must exist") 
+    end
+  end
+  
 end
