d51d5e3f15fa1b71b524c94246c437a0409e6336
moriken4320 <moriken.4320@gmail.com>
2020-12-07 14:16:04 +0900

    ルーム画面に遷移した際、一番したまでスクロールされた状態になる機能実装

--- app/javascript/messages/scroll.js
+++ app/javascript/messages/scroll.js
@@ -1,2 +1,5 @@
-// const chat_main = $("#chat-main");
-// $("#chat-main").scrollIntoView(false);
+$(function(){
+  if($(".main-room").length > 0){
+    $(".main-room")[0].scrollIntoView(false);
+  }
+});
