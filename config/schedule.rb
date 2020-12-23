a2e24b6cb60ad6319ed236b2a7aa95e8286eb7d2
moriken4320 <moriken.4320@gmail.com>
2020-12-23 19:18:54 +0900

    本番環境のcrontab修正

--- config/schedule.rb
+++ config/schedule.rb
@@ -6,19 +6,6 @@ job_type :rake, "export AWS_SECRET_ACCESS_KEY=\"#{ENV['AWS_SECRET_ACCESS_KEY']}\
 
 
 # AM5:00に全募集を削除する（JSTは+9:00なので-9:00の時間を記述）
-# every 1.day, at: '08:00 pm' do
-every 1.day, at: '10:17 am' do
+every 1.day, at: '08:00 pm' do
     rake "recruit_task:delete_all"
 end
-
-# 設定の確認
-# bundle exec whenever 
-
-# cronにデータを反映
-# bundle exec whenever --update-crontab 
-
-# cronからデータを削除
-# bundle exec whenever --clear-crontab
-
-# 反映したか確認
-# crontab -l 
